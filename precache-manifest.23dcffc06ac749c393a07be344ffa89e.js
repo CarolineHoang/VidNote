4ed995321605743f581863a9d11dfdf110c018da
Cremebrutale <caroline.hoang@columbiaspectator.com>
2020-08-15 21:39:09 -0400

    Updates

---
+++ precache-manifest.23dcffc06ac749c393a07be344ffa89e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37abb77be43a4fb8a104b62c615e3ad",
+    "url": "/VidNote/index.html"
+  },
+  {
+    "revision": "c508f13a7c2ead46c736",
+    "url": "/VidNote/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a66882f75d19f6ef5afa",
+    "url": "/VidNote/static/js/2.2ed74883.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VidNote/static/js/2.2ed74883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c508f13a7c2ead46c736",
+    "url": "/VidNote/static/js/main.4f018534.chunk.js"
+  },
+  {
+    "revision": "38b3b05932bd96f2b86f",
+    "url": "/VidNote/static/js/runtime-main.ac2d05ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VidNote/static/media/logo.5d5d9eef.svg"
+  }
+]);
