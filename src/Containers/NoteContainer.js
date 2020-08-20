8cb64470f89dd65a05a8b10e8daf02eedd155918
Cremebrutale <caroline.hoang@columbiaspectator.com>
2020-08-20 08:51:42 -0400

    currently having issues runing the developement server after an er an infinite loop error while setting up the search function, commiting the code to be safe
    Actual commit info: I created a button that will focus on the note that is most caught up with the video.

--- src/Containers/NoteContainer.js
+++ src/Containers/NoteContainer.js
@@ -144,25 +144,61 @@ export default class NoteContainer extends React.Component{
     constructor(props){
         super(props);
         this.state= {
-            index: 0
+            index: 0,
+            currentNoteId: 0
 
             // notes:
         }
         this.handleShow = this.handleShow.bind(this);
+        this.findMostRecentNoteIdx =this.findMostRecentNoteIdx.bind(this)
  
     }
-    handleShow(i) {
-        console.log(this.refs, i)
-        console.log(this.refs[toString(i)])
-        // this.setState({index: i});
-        
-        // this.refs[i].scrollIntoView({block: 'end', behavior: 'smooth'});
-        // console.log(this.refs[i].)
-        // this.refs[i].attributes.push("centerListItem")
-        console.log('the refs: ', this.refs, this.refs[1] )
-        this.refs[i].scrollIntoView({block: 'center', behavior: 'smooth'});
 
-        this.setState({index: i})
+    findMostRecentNoteIdx(){
+        var currentTime = this.props.getCurrVidTime()
+        var idx=0;
+        var notesArr = this.props.itemList.notes
+        if (notesArr.length == 0){
+            return -1 //error: there are no notes
+        }
+        if (notesArr.length == 1){
+            return 0
+        }
+        while (idx < notesArr.length){
+            console.log("time comparisons: ", notesArr[idx].startTime,  currentTime)
+
+            if(notesArr.length > 1 && idx < notesArr.length-1 ){
+                console.log("time comparisons: ", notesArr[idx].startTime,  currentTime)
+                if ( notesArr[idx].startTime <= currentTime && notesArr[idx+1].startTime > currentTime ){
+                    return idx
+                }
+            }
+            idx+=1
+        }
+        return notesArr.length-1
+    }
+    handleShow(command, i) {
+        if (command == "current"){
+           var mRIdx = this.findMostRecentNoteIdx()
+           if (mRIdx>0){
+                this.refs[mRIdx].scrollIntoView({block: 'center', behavior: 'smooth'});
+                console.log('mRIdx: ', mRIdx)
+                this.setState({index: mRIdx})
+           }
+        }
+        else{
+            console.log(this.refs, i)
+            console.log(this.refs[toString(i)])
+            // this.setState({index: i});
+            
+            // this.refs[i].scrollIntoView({block: 'end', behavior: 'smooth'});
+            // console.log(this.refs[i].)
+            // this.refs[i].attributes.push("centerListItem")
+            console.log('the refs: ', this.refs, this.refs[1] )
+            this.refs[i].scrollIntoView({block: 'center', behavior: 'smooth'});
+
+            this.setState({index: i})
+        }
       }
 
       
@@ -216,10 +252,12 @@ export default class NoteContainer extends React.Component{
                       
                     })}
                   </ul>
-                  <button onClick={this.handleShow.bind(this, 0)}>0</button>
-                  <button onClick={this.handleShow.bind(this, 2)}>2</button>
-                  <button onClick={this.handleShow.bind(this, 50)}>50</button>
-                  <button onClick={this.handleShow.bind(this, 99)}>99</button>
+                  <button onClick={this.handleShow.bind(this, "", 0)}>0</button>
+                  <button onClick={this.handleShow.bind(this, "", 2)}>2</button>
+                  <button onClick={this.handleShow.bind(this, "", 50)}>50</button>
+                  <button onClick={this.handleShow.bind(this, "", 99)}>99</button>
+                  <button onClick={this.handleShow.bind(this, "current", 0  )}>Current Note</button>
+
                   {this.state.index}
                 </div>
         
